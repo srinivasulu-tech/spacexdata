f2be6b0b7682f641ff554c4ecef403479d9a5494
srinivasulu-tech <66288005+srinivasulu-tech@users.noreply.github.com>
2020-09-03 08:03:40 +0530

    updated updated

--- src/index.js
+++ src/index.js
@@ -4,10 +4,9 @@ import ReactDOM from "react-dom";
 import Card from '@material-ui/core/Card';
 
 import CardContent from '@material-ui/core/CardContent';
-
-
+import {flexbox} from '@material-ui/system';
 import App from "./App";
-import { CardMedia } from "@material-ui/core";
+import { CardMedia, Typography} from "@material-ui/core";
 
 
 
@@ -35,130 +34,27 @@ class App1 extends Component {
         </center>
 
         {this.state.spacexlaunchs.map((data) => (
-         <Card >
-            <CardMedia image={data.flickr_images}>              
-            </CardMedia>
-          <CardContent> 
-              {data.flight_number}
-           </CardContent>
-         </Card>        
-         ))},
-         {this.state.spacexlaunchs.map((data) => (
+<flexbox  flexDirection="row" flexWrap="wrap" justifyContent="flex-start">
          <Card >
             <CardMedia image={data.flickr_images}>              
             </CardMedia>
           <CardContent> 
               {data.mission_name}
+              <Typography gutterBottom variant="headline" component ="h2">
+                 {data.mission_id}
+                </Typography>
+                <Typography gutterBottom variant="headline" component ="h2">
+                {data.launch_year}
+                </Typography>
+                <Typography gutterBottom variant="headline" component ="h2">
+                 {data.land_success}
+                </Typography>
+                <Typography gutterBottom variant="headline" component ="h2">
+                  {data.launch_success}
+                </Typography>
            </CardContent>
-         </Card>        
-         ))},
-         {this.state.spacexlaunchs.map((data) => (
-         <Card >
-            <CardMedia image={data.flickr_images}>              
-            </CardMedia>
-          <CardContent> 
-              {data.mission_id}
-           </CardContent>
-         </Card>        
-         ))},
-         {this.state.spacexlaunchs.map((data) => (
-         <Card >
-            <CardMedia image={data.flickr_images}>              
-            </CardMedia>
-          <CardContent> 
-              {data.upcoming}
-           </CardContent>
-         </Card>        
-         ))},
-         {this.state.spacexlaunchs.map((data) => (
-         <Card >
-            <CardMedia image={data.flickr_images}>              
-            </CardMedia>
-          <CardContent> 
-              {data.launch_year}
-           </CardContent>
-         </Card>        
-         ))},
-         {this.state.spacexlaunchs.map((data) => (
-         <Card >
-            <CardMedia image={data.flickr_images}>              
-            </CardMedia>
-          <CardContent> 
-              {data.launch_date_unix}
-           </CardContent>
-         </Card>        
-         ))},
-         {this.state.spacexlaunchs.map((data) => (
-         <Card >
-            <CardMedia image={data.flickr_images}>              
-            </CardMedia>
-          <CardContent> 
-              {data.launch_date_utc}
-           </CardContent>
-         </Card>        
-         ))},
-         {this.state.spacexlaunchs.map((data) => (
-         <Card >
-            <CardMedia image={data.flickr_images}>              
-            </CardMedia>
-          <CardContent> 
-              {data.launch_date_local}
-           </CardContent>
-         </Card>        
-         ))},
-         {this.state.spacexlaunchs.map((data) => (
-         <Card >
-            <CardMedia image={data.flickr_images}>              
-            </CardMedia>
-          <CardContent> 
-              {data.is_tentative}
-           </CardContent>
-         </Card>        
-         ))},
-         {this.state.spacexlaunchs.map((data) => (
-         <Card >
-            <CardMedia image={data.flickr_images}>              
-            </CardMedia>
-          <CardContent> 
-              {data.tentative_max_precision}
-           </CardContent>
-         </Card>        
-         ))},
-         {this.state.spacexlaunchs.map((data) => (
-         <Card >
-            <CardMedia image={data.flickr_images}>              
-            </CardMedia>
-          <CardContent> 
-              {data.tbd}
-           </CardContent>
-         </Card>        
-         ))},
-         {this.state.spacexlaunchs.map((data) => (
-         <Card >
-            <CardMedia image={data.flickr_images}>              
-            </CardMedia>
-          <CardContent> 
-              {data.launch_window}
-           </CardContent>
-         </Card>        
-         ))},
-         {this.state.spacexlaunchs.map((data) => (
-         <Card >
-            <CardMedia image={data.flickr_images}>              
-            </CardMedia>
-          <CardContent> 
-              {data.rocket_id}
-           </CardContent>
-         </Card>        
-         ))},
-         {this.state.spacexlaunchs.map((data) => (
-         <Card >
-            <CardMedia image={data.flickr_images}>              
-            </CardMedia>
-          <CardContent> 
-              {data.rocket_name}
-           </CardContent>
-         </Card>        
+         </Card>  
+         </flexbox>      
          ))}
 
       </div>
